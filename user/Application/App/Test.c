1f822ef0ffd462496bdb395134a87a37d80f1f7d
hxzp <ya!>
2025-07-27 19:12:13 +0800

    feat: 鼠标能够正常使用了

--- user/Application/App/Test.c
+++ user/Application/App/Test.c
@@ -41,19 +41,22 @@ void StartDefaultTask(void *argument)
   /* USER CODE BEGIN StartDefaultTask */    
   for(;;)
   {
-    // 按下A键
-    uint8_t key_report[8] = {0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00};
-    USBD_HID_SendReport(&hUsbDeviceFS, key_report, 8, 0);
-
-    // 保持按下状态一段时间（可选）
-    HAL_Delay(50);  // 50ms按键持续时间
-
-    // 释放所有按键
-    memset(key_report, 0, sizeof(key_report));
-    USBD_HID_SendReport(&hUsbDeviceFS, key_report, 8, 0);
-    
-    
     osDelay(1000);
+    
+    // 按下A键
+//    uint8_t key_report[8] = {0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00};
+//    USBD_HID_SendReport(&hUsbDeviceFS, key_report, 8, 0);
+
+//    // 保持按下状态一段时间（可选）
+//    HAL_Delay(50);  // 50ms按键持续时间
+
+//    // 释放所有按键
+//    memset(key_report, 0, sizeof(key_report));
+//    USBD_HID_SendReport(&hUsbDeviceFS, key_report, 8, 0);
+    
+    uint8_t mouse_report[4] = {0x00, 20, 15, 0};
+    USBD_HID_SendReport(&hUsbDeviceFS, mouse_report, 4, 1);    
+    
   }
   /* USER CODE END StartDefaultTask */
 }
